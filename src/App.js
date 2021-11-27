460036364d2b9eb59dc4758b65e2fd9b90e671dc
perchespierri <perchespierri@gmail.com>
2021-11-27 10:06:06 -0300

    Some style for visualization and initial form stages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
-import Form from './components/Form';
-import Timer from './components/Timer';
-import Finished from './components/Finished';
+import React, { useState } from "react";
+import Form from "./components/Form";
+import Timer from "./components/Timer";
+import Finished from "./components/Finished";
+import styled from 'styled-components';
 
 function App() {
   const [timeLeft, setTimeLeft] = useState(0);
   const [isPaused, setIsPaused] = useState(true);
-  
+
+  const Title = styled.div`
+    text-align: center;
+    font-size: 50px;
+    padding: 50px 0px;
+    background: pink;
+    color: whitesmoke;
+    text-shadow: 3px 3px grey;
+    margin-bottom: 20px; 
+  `;
+
   return (
     <>
+      <Title>Timer App</Title>
       <Form setTimeLeft={setTimeLeft} />
-      {timeLeft ?
-          <Timer
-            timeLeft={timeLeft}
-            setTimeLeft={setTimeLeft}
-            isPaused={isPaused}
-            setIsPaused={setIsPaused}
-          /> :
-          <Finished
-            setTimeLeft={setTimeLeft}
-            setIsPaused={setIsPaused}
-          />
+      {timeLeft < 0 ? 
+        <Finished
+          setTimeLeft={setTimeLeft}
+          setIsPaused={setIsPaused}
+        /> : 
+        <Timer
+          timeLeft={timeLeft}
+          setTimeLeft={setTimeLeft}
+          isPaused={isPaused}
+          setIsPaused={setIsPaused}
+        />
       }
     </>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
 
 const Form = () => {
+  const Container = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+
+  const Text = styled.div`
+    margin: 0px 0px;
+  `;
+
   return (
-    <div>
-      Form
-    </div>
+      <Container>
+        <Text>Minutes:</Text>
+        <form>
+          <input type="number" />
+          <input type="submit" />
+        </form>
+      </Container>  
   )
 }
 
