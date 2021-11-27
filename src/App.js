284dffb2ab951ef5443e5b7fd5772bb4dfe1a9b5
perchespierri <perchespierri@gmail.com>
2021-11-27 09:48:45 -0300

    Initial component logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Timer from './components/Timer';
+import Finished from './components/Finished';
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isPaused, setIsPaused] = useState(true);
+  
   return (
     <>
-      THE APP WORKS...FOR NOW.
+      <Form setTimeLeft={setTimeLeft} />
+      {timeLeft ?
+          <Timer
+            timeLeft={timeLeft}
+            setTimeLeft={setTimeLeft}
+            isPaused={isPaused}
+            setIsPaused={setIsPaused}
+          /> :
+          <Finished
+            setTimeLeft={setTimeLeft}
+            setIsPaused={setIsPaused}
+          />
+      }
     </>
   );
 }
