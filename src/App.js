e244c7ea7d3e5ed22017474d7764024c9870cc0f
perchespierri <perchespierri@gmail.com>
2021-11-27 10:23:18 -0300

    Fix problems with useForm hook

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Timer from "./components/Timer";
 import Finished from "./components/Finished";
 import styled from 'styled-components';
 
+const Title = styled.div`
+text-align: center;
+font-size: 50px;
+padding: 50px 0px;
+background: pink;
+color: whitesmoke;
+text-shadow: 3px 3px grey;
+margin-bottom: 20px; 
+`;
+
 function App() {
   const [timeLeft, setTimeLeft] = useState(0);
   const [isPaused, setIsPaused] = useState(true);
-
-  const Title = styled.div`
-    text-align: center;
-    font-size: 50px;
-    padding: 50px 0px;
-    background: pink;
-    color: whitesmoke;
-    text-shadow: 3px 3px grey;
-    margin-bottom: 20px; 
-  `;
 
   return (
     <>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,34 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
-const Form = () => {
-  const Container = styled.div`
-    display: flex;
-    justify-content: center;
-  `;
+const Container = styled.div`
+display: flex;
+justify-content: center;
+`;
 
-  const Text = styled.div`
-    margin: 0px 0px;
-  `;
+const Text = styled.div`
+margin: 0px 0px;
+`;
+
+
+const Form = () => {
+  const { register, handleSubmit, errors } = useForm();
 
   return (
       <Container>
         <Text>Minutes:</Text>
-        <form>
-          <input type="number" />
+        <form noValidate>
+          <input 
+            type="number" 
+            {...register('test', {
+              required: {value: true, message: "Please enter the number of minutes"},
+              min: {value: .01, message: "Please enter a number between 0.01 and 1000"},
+              max:{value: 1000, message: "Please enter a number between 0.01 and 1000"},
+            })} 
+          />
           <input type="submit" />
         </form>
+        {errors && <p>{errors.message}</p>}
       </Container>  
   )
 }
